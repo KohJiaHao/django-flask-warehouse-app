a704d24d9ed8ed96c0d257a565166018b443fafd
KohJiaHao <kohjiahao42@gmail.com>
2024-02-01 13:06:25 +0800

    added accounts/logout/

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.urls import reverse
+
 
 def index(request):
     return render(request, 'index.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('login'))
